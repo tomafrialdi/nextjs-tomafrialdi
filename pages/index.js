cbe3bfefba22a2da9d307fe722334ae7de357afc
Tom Afrialdi <tom.afrialdi@gmail.com>
2021-03-04 09:47:39 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Project of <a href="https://kotakpintar.com/layanan-tanya-veronika-asisten-virtual-telkomsel/"> Tanya Veronika Asisten Virtual!</a>
+          Welcome to Project of <a href="https://kotakpintar.com/layanan-tanya-veronika-asisten-virtual-telkomsel/"> Tanya Veronika Asisten Virtual</a>
         </h1>
 
         <p className={styles.description}>
